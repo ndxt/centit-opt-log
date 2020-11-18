5f219613491aece1273693e11fa0885c60f92681
杨淮生 <codefan@centit.com>
2020-11-18 16:57:01 +0800

    Update createTable.sql

--- opt-log-module/src/sqlscript/createTable.sql
+++ opt-log-module/src/sqlscript/createTable.sql
@@ -3,18 +3,18 @@
 -- Create table
 create table F_OPT_LOG
 (
-  log_id         VARCHAR2(32) not null,
-  log_level      VARCHAR2(2) not null,
-  user_code      VARCHAR2(8) not null,
-  opt_time       DATE not null,
-  opt_content    VARCHAR2(1000) not null,
-  new_value      CLOB,
-  old_value      CLOB,
-  opt_id         VARCHAR2(64) not null,
-  opt_method     VARCHAR2(64),
-  opt_tag        VARCHAR2(200),
-  correlation_id VARCHAR2(32),
-  unit_code      VARCHAR2(32),
+  LOG_ID         VARCHAR(32) NOT NULL,
+  LOG_LEVEL      VARCHAR(2) NOT NULL,
+  USER_CODE      VARCHAR(8) NOT NULL,
+  OPT_TIME       DATE NOT NULL,
+  OPT_CONTENT    VARCHAR(1000) NOT NULL,
+  NEW_VALUE      CLOB, -- text, 
+  OLD_VALUE      CLOB, -- text,
+  OPT_ID         VARCHAR(64) NOT NULL,
+  OPT_METHOD     VARCHAR(64),
+  OPT_TAG        VARCHAR(200),
+  CORRELATION_ID VARCHAR(32),
+  UNIT_CODE      VARCHAR(32),
   constraint PK_F_OPT_LOG primary key (LOG_ID)
 );
 
