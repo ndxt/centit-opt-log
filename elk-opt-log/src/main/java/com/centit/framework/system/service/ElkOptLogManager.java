40d6143acd15c1e4a98cb4118f138c591646d6bc
codefan <codefan@sina.com>
2021-04-20 16:48:36 +0800

    to:周抄 添加两个接口函数，并进行替换测试

--- elk-opt-log/src/main/java/com/centit/framework/system/service/ElkOptLogManager.java
+++ elk-opt-log/src/main/java/com/centit/framework/system/service/ElkOptLogManager.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service("elkOptLogManager")
 public class ElkOptLogManager implements OperationLogWriter {
@@ -43,6 +44,25 @@ public class ElkOptLogManager implements OperationLogWriter {
         if (esObjectIndexer.mergeDocument(esOperationLog) == null) {
             throw new ObjectException(500, "elasticsearch操作失败");
         }
+    }
+
+    /*
+     * 日志精确查询
+     * Map<String, Object> filterMap
+     * 中为条件 sql ，key为字段，格式为 字段代码_表达式， 这个比较难实现
+     * @see com.centit.support.database.jsonmaptable.buildFilterSql
+     */
+    @Override
+    public List<? extends OperationLog> listOptLog(String optId, Map<String, Object> filterMap, int startPos, int maxRows){
+        return null;
+    }
+
+    /*
+     * 查询日志数量，这个不知道是否可以做到
+     */
+    @Override
+    public int countOptLog(String optId, Map<String, Object> filter){
+        return -1;
     }
 
 }
